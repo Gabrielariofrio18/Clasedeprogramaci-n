5be1773227d4563a0b70c3e5d4624d3fdc35f8f2
gabrielariofrio <netoblion2000@gmail.com>
2022-10-28 10:33:14 -0500

    first commit

---
+++ src/Contenido.java
@@ -0,0 +1,26 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("variables");
+        System.out.println("Tipos de datos de nombres");
+        System.out.println("INT promedio");
+        System.out.println("Chat nombre");
+        System.out.println("Tipos de datos");
+        System.out.println("Enteros");
+        System.out.println("Reales");
+        System.out.println("Cadena de texto");
+        System.out.println("Caractér");
+        System.out.println("Booleano");
+        System.out.println("Operadores");
+        System.out.println("-");
+        System.out.println("+");
+        System.out.println("*");
+        System.out.println("/");
+        System.out.println("%");
+        System.out.println("Elementos básicos de un programa");
+        System.out.println("Palabras reservadas");
+        System.out.println("Class");
+        System.out.println("INT");
+        System.out.println("main");
+        System.out.println("Return");
+    }
+}
